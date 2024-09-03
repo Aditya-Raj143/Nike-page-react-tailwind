1353747542b442acb3fa0e19f72222d99295febd
Aditya-Raj143 <adityaxraj72004@gmail.com>
2024-09-03 17:32:12 +0530

    add a customer review section

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,9 +1,19 @@
+import ReviewCard from "../components/ReviewCard"
+import { reviews } from "../constants"
 
 const CustomerReviews = () => {
   return (
-    <div>
-      <h1></h1>
-    </div>
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold ">What Our
+        <span className="text-coral-red "> Customers</span> Say
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab sunt eius odio debitis nulla odit?</p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review)=> (
+          <ReviewCard key={review.customerName} imgURL={review.imgURL} customerName={review.customerName} rating={review.rating} feedback={review.feedback} />
+        ))}
+      </div>
+    </section>
   )
 }
 
